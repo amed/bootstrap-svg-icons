079ea5e6e6a35a755ac9adfc96118d0b232e508c
Amed Kino <amed.kino9@gmail.com>
2021-12-12 14:14:52 +0100

    Init gem :rocket:

---
+++ lib/bootstrap/svg/icons.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "icons/version"
+
+module Bootstrap
+  module Svg
+    module Icons
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bootstrap/svg/icons/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Bootstrap
+  module Svg
+    module Icons
+      VERSION = "0.1.0"
+    end
+  end
+end
